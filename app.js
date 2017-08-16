0e4d3b7f7c711ddcb370afefe53036de5eaefa52
ginniehench <ginniehench@gmail.com>
2017-08-15 22:41:37 -0400

    rendered page looks like the mockup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const path = require('path');
+let packages = "";
+let template = [];
 
 
 const app = express();
@@ -9,22 +11,48 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+// let todoArray = ["Node basics", "Express basics","Mustache","HTML forms with Express", "about authentication", "how to connect to PostgreSQL", "how to create databases", "SQL", "how to connect to PostgreSQL from Node", "how to connect to use Sequelize"]
+//
+// for (let i=0; i < packages.length; i++){
+//   let template = (`{"packages": "Learn " + todoArray[i]}`);
+// }
+
+//
+// Template:
+//
+// {{#person?}}
+//   Hi {{name}}!
+// {{/person?}}
+// Hash:
+//
+// {
+//   "person?": { "name": "Jon" }
+// }
+// Output:
+//
+// Hi Jon!
+
+// Mustache.render('<ul>{{#.}}<li>{{.}}</li>{{/.}}</ul>', ['foo','bar','baz']);
+
 //Listening on root
 app.get('/todo/', function (req, res){
   res.render('todo', {
-    "option_1": [
-      {"packages": "Node basics"},
-      {"packages": "Express basics"},
-      {"packages": "Mustache"},
-
-      {"packages": "HTML forms with Express"},
-      {"packages": "about authentication"},
-      {"packages": "how to connect to PostgreSQL"},
-      {"packages": "how to create databases"},
-      {"packages": "SQL"},
-      {"packages": "how to connect to PostgreSQL from Node"},
-      {"packages": "how to connect to use Sequelize"},
-    ]
+       "option_1": {
+              "completed": [
+                {"packages": "Node basics"},
+                {"packages": "Express basics"},
+                {"packages": "Mustache"}
+              ],
+              "incomplete": [
+                {"packages": "HTML forms with Express"},
+                {"packages": "about authentication"},
+                {"packages": "how to connect to PostgreSQL"},
+                {"packages": "how to create databases"},
+                {"packages": "SQL"},
+                {"packages": "how to connect to PostgreSQL from Node"},
+                {"packages": "how to connect to use Sequelize"}
+              ]
+          }
   })
 });
 
